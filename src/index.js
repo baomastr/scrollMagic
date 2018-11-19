40f75b1f2a911c834005e08790896ebc448d3a0d
Dmitriy Lisyanskiy <d.lisyanskiy@tinkoff.ru>
2018-11-19 15:00:24 +0300

    add two scenes

--- src/index.js
+++ src/index.js
@@ -14,29 +14,92 @@ const
   pageWidth = window.innerWidth || element.clientWidth || page.clientWidth,
   pageHeight = window.innerHeight || element.clientHeight || page.clientHeight;
 
-const flightpath = {
-  entry: {
-    // curviness: 1.25,
-    // autoRotate: true,
-    values: [{x: 0, y: 0}, {x: 300, y: 0}]
-  },
-  looping: {
-    // curviness: 1.25,
-    // autoRotate: true,
+const onCompleteFunc = (item) => () => {
+  console.log(item);
+};
+
+const sceneEventFunc = (event) => {
+  const logger = () => console.log("Event fired! (" + event.type + ")");
+
+  if (event.type === 'start' || event.type === 'leave' || event.type === 'end') {
+    logger()
+  }
+};
+
+const flightpath1 = {
+  '1': {
     values: [
-      {x: 510, y: 0},
-      {x: 620, y: 0},
-      {x: 500, y: 0},
-      {x: 620, y: 0}
+      {x: 0, onComplete: onCompleteFunc('1 started'),},
+      {x: 280},
     ]
   },
-  leave: {
-    // curviness: 1.25,
-    // autoRotate: true,
+  '2': {
     values: [
-      {x: 660, y: 0},
-      {x: 400, y: 0},
-      {x: 0, y: 0}
+      {x: 280, onComplete: onCompleteFunc('2 started'),},
+      {x: 94},
+    ]
+  },
+  '3': {
+    values: [
+      {x: 94, onComplete: onCompleteFunc('3 started'),},
+      {x: -60},
+    ]
+  },
+  '4': {
+    values: [
+      {x: -60, onComplete: onCompleteFunc('4 started'),},
+      {x: -230},
+    ]
+  },
+  '5': {
+    values: [
+      {x: -230, onComplete: onCompleteFunc('5 started'),},
+      {x: -335},
+    ]
+  },
+  '6': {
+    values: [
+      {x: -335, onComplete: onCompleteFunc('6 started'),},
+      {x: 0},
+    ]
+  }
+};
+
+const flightpath2 = {
+  '1': {
+    values: [
+      {x: 0, onComplete: onCompleteFunc('1 started'),},
+      {x: 280},
+    ]
+  },
+  '2': {
+    values: [
+      {x: 280, onComplete: onCompleteFunc('2 started'),},
+      {x: 94},
+    ]
+  },
+  '3': {
+    values: [
+      {x: 94, onComplete: onCompleteFunc('3 started'),},
+      {x: -60},
+    ]
+  },
+  '4': {
+    values: [
+      {x: -60, onComplete: onCompleteFunc('4 started'),},
+      {x: -230},
+    ]
+  },
+  '5': {
+    values: [
+      {x: -230, onComplete: onCompleteFunc('5 started'),},
+      {x: -335},
+    ]
+  },
+  '6': {
+    values: [
+      {x: -335, onComplete: onCompleteFunc('6 started'),},
+      {x: 0},
     ]
   }
 };
@@ -44,59 +107,123 @@ const flightpath = {
 class App extends Component {
   constructor(props) {
     super(props);
-    this.controller = new ScrollMagic.Controller();
+    this.controller1 = new ScrollMagic.Controller();
+    this.controller2 = new ScrollMagic.Controller();
     // this.scene = new ScrollMagic.Scene();
-    this.tween = new TimelineMax();
+    this.tween1 = new TimelineMax();
+    this.tween2 = new TimelineMax();
     this.ball = React.createRef();
-    this.trigger = React.createRef();
-    this.target = React.createRef();
+    this.trigger1 = React.createRef();
+    this.trigger2 = React.createRef();
+    this.target1 = React.createRef();
+    this.target2 = React.createRef();
   }
 
   componentDidMount() {
-    const sceneConfig = {triggerElement: this.trigger, duration: 1510, offset: 0};
+    const scene1Config = {triggerElement: this.trigger1.current, duration: 1080, offset: 388};
+    const scene2Config = {triggerElement: this.trigger2.current, duration: 1080, offset: 388};
 
-    const tween = this.tween
-      .add(
-        TweenMax.to(this.ball.current, 1.2, {
-          css: {bezier: flightpath.entry},
-          // ease: Power1.easeInOut
-        })
-      )
-      .add(
-        TweenMax.to(this.ball.current, 2, {
-          css: {bezier: flightpath.looping},
-          // ease: Power1.easeInOut
-        })
-      )
-      .add(
-        TweenMax.to(this.ball.current, 1, {
-          css: {bezier: flightpath.leave},
-          // ease: Power1.easeInOut
-        })
-      );
+    const tween1 = this.tween1
+      .add(TweenMax.to(this.ball.current, 1.13, {
+            css: {bezier: flightpath1['1']},
+          })
+        )
+        .add(
+          TweenMax.to(this.ball.current, 1.36, {
+            css: {bezier: flightpath1['2']},
+          })
+        )
+        .add(
+          TweenMax.to(this.ball.current, .61, {
+            css: {bezier: flightpath1['3']},
+          })
+        )
+        .add(
+          TweenMax.to(this.ball.current, 1.05, {
+            css: {bezier: flightpath1['4']},
+          })
+        )
+        .add(
+          TweenMax.to(this.ball.current, .61, {
+            css: {bezier: flightpath1['5']},
+          })
+        )
+        .add(
+          TweenMax.to(this.ball.current, 1.2, {
+            css: {bezier: flightpath1['6']},
+          })
+        );
 
-    new ScrollMagic.Scene(sceneConfig)
-      .setPin(this.target.current)
-      .setTween(tween)
+    const tween2 = this.tween2
+      .add(TweenMax.to(this.ball.current, 1.13, {
+            css: {bezier: flightpath2['1']},
+          })
+        )
+        .add(
+          TweenMax.to(this.ball.current, 1.36, {
+            css: {bezier: flightpath2['2']},
+          })
+        )
+        .add(
+          TweenMax.to(this.ball.current, .61, {
+            css: {bezier: flightpath2['3']},
+          })
+        )
+        .add(
+          TweenMax.to(this.ball.current, 1.05, {
+            css: {bezier: flightpath2['4']},
+          })
+        )
+        .add(
+          TweenMax.to(this.ball.current, .61, {
+            css: {bezier: flightpath2['5']},
+          })
+        )
+        .add(
+          TweenMax.to(this.ball.current, 1.2, {
+            css: {bezier: flightpath2['6']},
+          })
+        );
+
+    new ScrollMagic.Scene(scene1Config)
+      .on("change update progress start end enter leave", sceneEventFunc)
+      .setPin(this.target1.current)
+      .setTween(tween1)
       .addIndicators() // add indicators (requires plugin)
-      .addTo(this.controller);
+      .addTo(this.controller1);
+
+     new ScrollMagic.Scene(scene2Config)
+      .on("change update progress start end enter leave", sceneEventFunc)
+      .setPin(this.target2.current)
+      .setTween(tween2)
+      .addIndicators() // add indicators (requires plugin)
+      .addTo(this.controller2);
   }
 
   render() {
     return (
       <div className="App">
         <AnimatedBg>
-          <div className="spacer"/>
-          <Transition key={1} height="400px" from="#0D47A1" to="#388E3C" />
-          <div className="s.target" ref="target">
-            <div className="ball" ref={this.ball}/>
+          {/*<Transition key={1} height="120px" from="#0D47A1" to="#388E3C"/>*/}
+
+          <div className="ball" ref={this.ball}/>
+
+          <div className="wrapper wrapper1">
+            <div className="trigger" ref={this.trigger1}/>
+            <div className="target" ref={this.target1}/>
           </div>
+
+
+          <div className="wrapper wrapper1">
+            <div className="trigger" ref={this.trigger2}/>
+            <div className="target" ref={this.target2}/>
+          </div>
+
           <div className="spacer"/>
-          <Transition key={2} height="400px" from="#388E3C" to="#FFA000" position={0.75}/>
-          <div className="trigger" ref={this.trigger}/>
           <div className="spacer"/>
-          <Transition key={3} height="400px" from="#FFA000" to="#0D47A1" position={0.75}/>
-          <div className="spacer"/>
+
+          {/*<Transition key={2} height="400px" from="#388E3C" to="#FFA000"/>*/}
+
         </AnimatedBg>
       </div>
     );
