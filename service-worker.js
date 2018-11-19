e9090a9638d25b8c4ae448158690d0a697fe78da
Dmitriy Lisyanskiy <d.lisyanskiy@tinkoff.ru>
2018-11-19 17:54:51 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/baomastr/scrollMagic/precache-manifest.ee6bcfa7f4c3d83f4d48d7c42bd8cfe9.js"
+  "/precache-manifest.2461f0eefde349127f0a50a35403abeb.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/baomastr/scrollMagic/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
