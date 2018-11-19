395c8e9e17479ed9b082fe89e63d00e6c3559b56
Dmitriy Lisyanskiy <d.lisyanskiy@tinkoff.ru>
2018-11-19 18:26:48 +0300

    Updates

---
+++ precache-manifest.9b7327024a9cf17f7f5ff39e053dc1a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "711e3dac986b99cd736224a33b17573e",
+    "url": "/scrollMagic/static/media/Slide_1_sheme.711e3dac.png"
+  },
+  {
+    "revision": "2af15b7175b47ee72c57",
+    "url": "/scrollMagic/static/js/runtime~main.2af15b71.js"
+  },
+  {
+    "revision": "29d407516aef1b38aead",
+    "url": "/scrollMagic/static/js/main.29d40751.chunk.js"
+  },
+  {
+    "revision": "146e7e7e1d6e111347f6",
+    "url": "/scrollMagic/static/js/1.146e7e7e.chunk.js"
+  },
+  {
+    "revision": "29d407516aef1b38aead",
+    "url": "/scrollMagic/static/css/main.3aa5c394.chunk.css"
+  },
+  {
+    "revision": "5c37dac13605c9ae9b472276124efdfa",
+    "url": "/scrollMagic/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2461f0eefde349127f0a50a35403abeb.js"
+  "/scrollMagic/precache-manifest.9b7327024a9cf17f7f5ff39e053dc1a7.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/scrollMagic/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
