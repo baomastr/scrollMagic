cd988dfb9ac9a2375080eb39f5212652ae6394b6
Dmitriy Lisyanskiy <d.lisyanskiy@tinkoff.ru>
2018-11-19 16:55:49 +0300

    add alias to webpack.config.prod

---
+++ build/precache-manifest.ee6bcfa7f4c3d83f4d48d7c42bd8cfe9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "711e3dac986b99cd736224a33b17573e",
+    "url": "/baomastr/scrollMagic/static/media/Slide_1_sheme.711e3dac.png"
+  },
+  {
+    "revision": "2f04dcc9e98276e3eb77",
+    "url": "/baomastr/scrollMagic/static/js/runtime~main.2f04dcc9.js"
+  },
+  {
+    "revision": "f9a3efaf819b48ca8bd7",
+    "url": "/baomastr/scrollMagic/static/js/main.f9a3efaf.chunk.js"
+  },
+  {
+    "revision": "146e7e7e1d6e111347f6",
+    "url": "/baomastr/scrollMagic/static/js/1.146e7e7e.chunk.js"
+  },
+  {
+    "revision": "f9a3efaf819b48ca8bd7",
+    "url": "/baomastr/scrollMagic/static/css/main.7a5575b9.chunk.css"
+  },
+  {
+    "revision": "967978a657992316d9c1d0bcb1bfb33b",
+    "url": "/baomastr/scrollMagic/index.html"
+  }
+];
